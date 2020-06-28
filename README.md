96e3dd9db6687ce15ba440cf4c903960d8f94c96
anzai.k <anzai.k@opst.co.jp>
2020-06-28 12:15:10 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Java solution for Leetcode
+Java solution of problems from [LeetCode](https://leetcode.com/).
+
+| # | Title | Solution | Difficulty |
+|---| ----- | -------- | ---------- |
+|1|[Two Sum](https://leetcode.com/problems/two-sum/)|[Java](./Solution1.java)|Easy|
---
+++ Solution1.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = new int[]{2, 7, 11, 13};
+        int target = 9;
+        System.out.println("Input: nums:" + Arrays.toString(nums));
+        System.out.println("Input: target:" + String.valueOf(target));
+
+        int[] answers =  solution.twoSum(nums, target);
+        System.out.println("Output: answers:" + Arrays.toString(answers));
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int answer_list[] = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int x = nums[i];
+                int y = nums[j];
+                if (x + y == target) {
+                    answer_list[0] = i;
+                    answer_list[1] = j;
+                    break;
+                }
+            }
+        }
+        return answer_list;
+    }
+}
