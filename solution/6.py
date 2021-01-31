e4d7cd8c0f646a2107a83e546b3933dd8986f35b
kumiizoo <kumii.zoo.935@gmail.com>
2021-01-31 12:40:31 +0900

    6が解き途中でAPIが必要

--- solution/6.py
+++ solution/6.py
@@ -9,11 +9,11 @@ class Solution:
     UA     M I     N M
     H       A       E
     ----------------
-    FMC
+    FM C
     UAUIHI
     YSINIU
     UAMINM
-    HAE
+    H A E
     ----------------
     Input: s = "FUYUHASAMUIMAINICHINEMUI", numRows = 5
     Output: "FMCUAUIHIYSINIUUAMINMHAE"
@@ -21,18 +21,24 @@ class Solution:
 
     @staticmethod
     def convert(s: str, num_rows: int) -> str:
-        string_length = len(s)
+        s_list = []
+        for i in range(num_rows):
+            s_list.append([])
+
+        s_length = len(s)
+        s_list_length = len(s_list)
+
+        for i in range(s_length):
+            for j in range(s_list_length):
+                s_list[j].append(s[i:i+1])
+
+        print(s_list)
+
         s_1 = s[0:5]
         s_2 = s[5:9][::-1]
         s_3 = s[9:13]
         s_4 = s[13:17][::-1]
         s_5 = s[17:21]
         s_6 = s[21:24][::-1]
-        print(s_1)
-        print(s_2)
-        print(s_3)
-        print(s_4)
-        print(s_5)
-        print(s_6)
 
 Solution.convert("FUYUHASAMUIMAINICHINEMUI", 5)
