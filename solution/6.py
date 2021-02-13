4dfd6180fd15da6d048b0eb74d904b38a53e2903
kumiizoo <kumii.zoo.935@gmail.com>
2021-02-13 12:27:46 +0900

    まだ３段構成が動かない

--- solution/6.py
+++ solution/6.py
@@ -17,6 +17,30 @@ class Solution:
     ----------------
     Input: s = "FUYUHASAMUIMAINICHINEMUI", numRows = 5
     Output: "FMCUAUIHIYSINIUUAMINMHAE"
+    ----------------
+    P  I     N
+    A L S   I G
+    YA   H R
+    P     I
+    ----------------
+    PI N
+    ALSIG
+    YAHR
+    P I
+    ----------------
+    Input: s = "PAYPALISHIRING", numRows = 4
+    Output: "PINALSIGYAHRPI"
+    ----------------
+    P  A   H   N
+    A P L S I I G
+    Y    I   R
+    ----------------
+    PA H N
+    APLSIIG
+    Y I R
+    ----------------
+    Input: s = "PAYPALISHIRING", numRows = 3
+    Output: ""
     """
 
     @staticmethod
@@ -27,30 +51,36 @@ class Solution:
         count = 0
 
         for i in range(s_length):
-            if i % num_rows != 0 and count * num_rows < s_length:
+            if i % num_rows != 0 and count * num_rows <= s_length:
+                print("A")
                 continue
 
             s_list.append([])
 
             if i == 0:
+                print("B")
                 s_list[count].append(s[i:i + num_rows - count])
                 count += 1
                 continue
 
-            if count % 2 == 0 and count * num_rows < s_length:
+            if count % 2 == 0 and count * num_rows <= s_length:
+                print("C")
                 s_list[count].append(str(0) + s[i - count + 1:i + num_rows - count])
 
             elif count % 2 == 0 and count * num_rows > s_length:
+                print("D")
                 s_list[count].append(str(0))
                 s_list[count][0] += s[i:s_length]
                 for j in range(num_rows - len(s[i:s_length]) - 1):
                     s_list[count][0] += str(0)
-                    break
+                break
 
-            elif count % 2 != 0 and count * num_rows < s_length:
+            elif count % 2 != 0 and count * num_rows <= s_length:
+                print("E")
                 s_list[count].append(s[i - count + 1:i + num_rows - count][::-1] + str(0))
 
             elif count % 2 != 0 and count * num_rows > s_length:
+                print("F")
                 for j in range(num_rows - len(s[i:s_length][::-1]) - 1):
                     if j == 0:
                         s_list[count].append(str(0))
@@ -68,9 +98,12 @@ class Solution:
                 if list(s_list[j][0])[i] != str(0):
                     converted_s += s_list[j][0][i]
 
+        print(s_list)
+        print(s)
+        print(len(s))
         print(converted_s)
         print(len(converted_s))
 
         return converted_s
 
-Solution.convert("FUYUHASAMUIMAINICHINEMUI", 5)
+Solution.convert("PAYPALISHIRING",3)
